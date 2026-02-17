e793cca65e19de402b6684eb54634c6a96b1cd4f
Damian <zarzad@ebuildo.pl>
2026-02-17 12:09:12 +0100

    Debug: Add banner properly and create /debug page

---
+++ apps/web/src/app/[locale]/debug/page.tsx
@@ -0,0 +1,7 @@
+export default function DebugPage() {
+    return (
+        <div className="p-20 bg-green-500 text-white font-bold text-4xl text-center">
+            DEBUG SITE IS WORKING!
+        </div>
+    );
+}
