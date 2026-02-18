47586c958d6fdad92c627d9e2868d065f2e3ffca
Damian <zarzad@ebuildo.pl>
2026-02-18 21:19:31 +0100

    feat(auth): add google provider configuration

--- apps/web/src/auth.config.ts
+++ apps/web/src/auth.config.ts
@@ -1,7 +1,15 @@
 import type { NextAuthConfig } from "next-auth"
 
+import Google from "next-auth/providers/google"
+
 export default {
-    providers: [],
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+            allowDangerousEmailAccountLinking: true,
+        }),
+    ],
     pages: {
         signIn: "/admin/login",
     },
